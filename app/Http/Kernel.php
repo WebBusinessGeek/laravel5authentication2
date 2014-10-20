3a863fbd8b9269852c526081c42371a8cd3ef9a3
Kevin Washington <kevw12188@gmail.com>
2014-10-20 17:23:42 -0400

    first commit

---
+++ app/Http/Kernel.php
@@ -0,0 +1,41 @@
+<?php namespace App\Http;
+
+use Exception;
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel {
+
+	/**
+	 * The application's HTTP middleware stack.
+	 *
+	 * @var array
+	 */
+	protected $middleware = [
+		'App\Http\Middleware\MaintenanceMiddleware',
+		'Illuminate\Cookie\Middleware\Guard',
+		'Illuminate\Cookie\Middleware\Queue',
+		'Illuminate\Session\Middleware\Reader',
+		'Illuminate\Session\Middleware\Writer',
+		'Illuminate\View\Middleware\ErrorBinder',
+		'App\Http\Middleware\CsrfMiddleware',
+	];
+
+	/**
+	 * Handle an incoming HTTP request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function handle($request)
+	{
+		try
+		{
+			return parent::handle($request);
+		}
+		catch (Exception $e)
+		{
+			throw $e;
+		}
+	}
+
+}
